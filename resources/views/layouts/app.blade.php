278e60e0d7b9c1a1c71e43fa3c2eeaf1a08b4664
hendisantika <hendisantika@yahoo.co.id>
2024-02-05 14:44:41 +0700

    Modify heading 🫸🌀✏️📗🐧🐳

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,19 @@
     @include('layouts.navigation')
 
     <!-- Page Heading -->
+    <!-- Page Heading -->
     @if (isset($header))
-        <header class="bg-white dark:bg-gray-800 shadow">
+        <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
+
+                {{-- check if there is a notif.success flash session --}}
+                @if (Session::has('notif.success'))
+                    <div class="bg-blue-300 mt-2 p-4">
+                        {{-- if it's there then print the notification --}}
+                        <span class="text-white">{{ Session::get('notif.success') }}</span>
+                    </div>
+                @endif
             </div>
         </header>
     @endif
