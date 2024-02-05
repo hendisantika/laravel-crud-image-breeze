0384c01defeceeae9f3878d1897bc1cd515ad590
hendisantika <hendisantika@yahoo.co.id>
2024-02-05 14:39:44 +0700

    Add rules in Post/UpdateRequest 🫸🌀✏️📗🐧🐳

--- app/Http/Requests/Post/UpdateRequest.php
+++ app/Http/Requests/Post/UpdateRequest.php
@@ -23,7 +23,9 @@ class UpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|min:3|max:250',
+            'content' => 'required|string|min:3|max:6000',
+            'featured_image' => 'nullable|image|max:1024|mimes:jpg,jpeg,png',
         ];
     }
 }
